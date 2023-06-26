72c3b1bfa45e96e0372ab466a21494840201b7fa
Isaac <isaac.ortega@alaskaair.com>
2023-06-26 10:07:46 -0700

    feat: introduce basic functionality

--- src/auro-sidenav.js
+++ src/auro-sidenav.js
@@ -11,50 +11,67 @@ import { LitElement, html } from "lit";
 // import { LitElement, html } from "lit";
 // import AuroElement from '@aurodesignsystem/webcorestylesheets/dist/auroElement/auroElement';
 
+// Register dependent components
+import './auro-sidenavsection';
+import './auro-sidenavitem';
+
 // Import touch detection lib
 import styleCss from "./style-css.js";
 
-// See https://git.io/JJ6SJ for "How to document your components using JSDoc"
 /**
- * The auro-sidenav element provides users a way to ... (it would be great if you fill this out).
- *
- * @attr {Boolean} fixed - Uses fixed pixel values for element shape
- * @attr {String} cssClass - Applies designated CSS class to demo element - you want to delete me!
+ * The auro-sidenav element provides users a way to create navigational interfaces on the lefthand
+ * side of a page to navigate to seperate pages or different sections within the same page.
+ * @slot heading - Defines what to use as the header of the sidenav
+ * @slot - Default slot of the sidenav. Please construct using auro-sidenavitems and auro-sidenavsections.
  */
 
 // build the component class
 export class AuroSidenav extends LitElement {
-  // constructor() {
-  //   super();
-  // }
 
-  // This function is to define props used within the scope of this component
-  // Be sure to review  https://lit.dev/docs/components/properties/
-  // to understand how to use reflected attributes with your property settings.
   static get properties() {
-    return {
-      // ...super.properties,
-
-      // this property is DEMO ONLY! Please delete.
-      cssClass:   { type: String }
-    };
+    return {};
   }
 
   static get styles() {
     return [styleCss];
   }
 
-  // When using auroElement, use the following attribute and function when hiding content from screen readers.
-  // aria-hidden="${this.hideAudible(this.hiddenAudible)}"
+  /**
+   * Used to update sidenavitem tiers on construction and @slotchange event.
+   * @private
+   */
+  handleSlotChange() {
+
+    /**
+     * Recursive helper function used to recurse through children and attach tiers to sidenavitems.
+     * @param {*} node Describes node to apply slot changes to.
+     * @param {*} depth Describes depth that specified `node` exists in relative DOM tree.
+     * @private
+     */
+    function handleSlotChangeHelper(node, depth) {
+      for (const element of node.children) {
+        if (element.tagName.includes('ITEM')) {
+          element.setAttribute('tier', depth);
+        } else if (element.tagName.includes('SECTION')) {
+          element.setAttribute("chevron", true);
+          element.setAttribute("fluid", true);
+          handleSlotChangeHelper(element, depth + 1);
+        }
+      }
+    }
+
+    handleSlotChangeHelper(this, 0);
+  }
+
+  firstUpdated() {
+    this.handleSlotChange();
+  }
 
   // function that renders the HTML and CSS into  the scope of the component
   render() {
     return html`
-
-      <!-- this is demo code, DO NOT USE IN YOUR ELEMENT -->
-      <div class=${this.cssClass} tabindex="0">
-        <slot></slot>
-      </div>
+      <span><slot name="heading"></slot></span>
+      <slot @slotchange="{this.handleSlotChange}"></slot>
     `;
   }
 }
---
+++ src/auro-sidenavsection.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2023 Alaska Airlines. All right reserved. Licensed under the Apache-2.0 license
+// See LICENSE in the project root for license information.
+
+// ---------------------------------------------------------------------
+
+import { AuroAccordion } from '@aurodesignsystem/auro-accordion/dist/auro-accordion';
+
+import styleCss from './style-sections-css.js';
+
+// build the component class
+export class AuroSideNavSection extends AuroAccordion {
+
+  static get styles() {
+    const styles = super.styles;
+    return [
+      ...styles,
+      styleCss
+    ];
+  }
+
+}
+
+// default internal definition
+if (!customElements.get("auro-sidenavsection")) {
+  customElements.define("auro-sidenavsection", AuroSideNavSection);
+}
