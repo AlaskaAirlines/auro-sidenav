6de464017b522699a61b5b7be5ddc613e10bfb9c
Isaac <isaac.ortega@alaskaair.com>
2023-07-31 12:15:59 -0700

    test: add test for dynamic selected attribute

--- test/auro-sidenav.test.js
+++ test/auro-sidenav.test.js
@@ -1,4 +1,4 @@
-import { fixture, html, expect } from '@open-wc/testing';
+import { fixture, html, expect, elementUpdated } from '@open-wc/testing';
 import '../src/auro-sidenav';
 
 describe('auro-sidenav', () => {
@@ -33,11 +33,50 @@ describe('auro-sidenav', () => {
     const levelTwoLink = el.querySelector(".levelTwoLink");
     const section = el.querySelector("auro-sidenavsection");
 
-    console.log(levelOneLink)
-
     await expect(levelOneLink).to.have.attr('tier', '0');
     await expect(levelTwoLink).to.have.attr('tier', '1');
     await expect(section).to.have.attr('chevron', 'true');
     await expect(section).to.have.attr('fluid', 'true');
+  });
+
+  it('auro-sidenav updates sidenavitem with selected attribute', async () => {
+
+    const el = await fixture(html`
+      <auro-sidenav>
+      <span slot="heading">Heading</span>
+      <auro-sidenavitem class="linkOne" selected>Link 1</auro-sidenavitem>
+      <auro-sidenavitem class="linkTwo">Link 2</auro-sidenavitem>
+      <auro-sidenavitem class="linkThree">Link 3</auro-sidenavitem>
+    </auro-sidenav>
+    `);
+
+    const linkOne = el.querySelector(".linkOne");
+    const linkTwo = el.querySelector(".linkTwo");
+    const linkThree = el.querySelector(".linkThree");
+
+    expect(linkOne).to.have.attr('selected');
+    expect(linkTwo).not.to.have.attr('selected');
+    expect(linkThree).not.to.have.attr('selected');
+
+    linkTwo.dispatchEvent(new Event('mousedown', {
+      bubbles: true,
+      composed: true,
+    }));
+    await elementUpdated(el);
+
+    expect(linkOne).not.to.have.attr('selected');
+    expect(linkTwo).to.have.attr('selected');
+    expect(linkThree).not.to.have.attr('selected');
+
+    linkThree.dispatchEvent(new KeyboardEvent('keydown', {
+      bubbles: true,
+      composed: true,
+      key: 'Enter'
+    }));
+    await elementUpdated(el);
+
+    expect(linkOne).not.to.have.attr('selected');
+    expect(linkTwo).not.to.have.attr('selected');
+    expect(linkThree).to.have.attr('selected');
   });
 });
