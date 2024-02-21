ff6b88a4fd09dd08311719cfadf08a14f28fb158
Jason Baker <jason@capsule42.com>
2024-02-20 20:33:01 -0800

    fix(registration): make custom registration rename all component parts

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AuroSidenav } from './src/auro-sidenav.js';
+import { AuroSideNavItem } from './src/auro-sidenavitem.js'; 
+import { AuroSideNavSection } from './src/auro-sidenavsection.js';
 
 /**
  * Register Custom Element.
@@ -6,10 +8,23 @@ import { AuroSidenav } from './src/auro-sidenav.js';
  * @returns {void}
  */
  const registerComponent = (name = 'custom-sidenav') => {
+  const itemName = name + 'item';
+  const sectionName = name + 'section';
+
   // alias definition
   if (!customElements.get(name)) {
     customElements.define(name, class extends AuroSidenav {});
   }
+
+  if (!customElements.get(itemName)) {
+    customElements.define(itemName, class extends AuroSideNavItem {});
+  }
+
+  if (!customElements.get(sectionName)) {
+    customElements.define(sectionName, class extends AuroSideNavSection {});
+  }
+
+  console.warn(name, itemName, sectionName);
 }
 
 export { registerComponent }
