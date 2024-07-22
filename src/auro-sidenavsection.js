96ff3cf227a30c3fccc5f2f7a2e17bc6e1011673
Dale Sande <dale.sande@gmail.com>
2024-07-22 15:31:39 -0700

    fix: update import reference path #28
    This commit updates the reference path to auro-accordion from the
    incorrect `./dist` path to the `./src` path.
    
    Reason why this was set to `./dist` and the element still operates as
    expected locally is unknown.
    
    On branch dsande/hotfix/refpath/28
    Changes to be committed:
    modified:   src/auro-sidenavsection.js

--- src/auro-sidenavsection.js
+++ src/auro-sidenavsection.js
@@ -3,7 +3,7 @@
 
 // ---------------------------------------------------------------------
 
-import { AuroAccordion } from '@aurodesignsystem/auro-accordion/dist/auro-accordion.js';
+import { AuroAccordion } from '@aurodesignsystem/auro-accordion/src/auro-accordion.js';
 
 import styleCss from './style-sections-css.js';
 
